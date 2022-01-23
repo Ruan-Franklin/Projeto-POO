dc725fa9b1dd936f85763494e8b1160dc495e9ea
RUAN FRANKLIN <94229859+Ruan-Franklin@users.noreply.github.com>
2022-01-23 01:18:23 +0000

    Pequena atualização

--- Cap_2_Turing/Main.cs
+++ Cap_2_Turing/Main.cs
@@ -12,7 +12,10 @@ class MainClass {
          opção=Menu();
          switch(opção) {
            case 1 : PagCliente(); break;
-           case 2 : PagVendedor(); break; }  }
+           case 2 : PagVendedor(); break;
+           case 3 : ProdutoListar(); break;
+           case 4 : ProdutoInserir(); break; }
+             }
        catch (Exception erro) {
          Console.WriteLine(erro.Message);
          opção=100; }
@@ -25,12 +28,15 @@ class MainClass {
 
      Console.WriteLine("1 - Cliente - Acessar área de clientes");
      Console.WriteLine("2 - Vendedor - Acessar área de vendedores");
+     Console.WriteLine("3-  Produto listar - Teste" );
+     Console.WriteLine("4-  Produto inserir - Teste" );
      Console.WriteLine("0 - Fim");
      Console.Write("Informe uma opção: ");
      int opção = int.Parse(Console.ReadLine());
      Console.WriteLine();
      return opção;                                                        }
    public static void PagCliente() {
+     int seleção=0;
      Console.WriteLine("----- Categorias de jogos disponíveis -----");
      Categoria[] ct = ncategoria.Listar();
      if(ct.Length==0){
@@ -38,9 +44,21 @@ class MainClass {
        return;}
      foreach(Categoria c in ct){
        Console.WriteLine(c);
-       Console.WriteLine();
+       Console.WriteLine(); }
+       do{
+         try{
+           seleção=Usuario();
+           switch(seleção) {
+             case 1 : ProdutoListar(); break;
+             case 2 : ProdutoInserir(); break;
      }
      }
+         catch (Exception erro) {
+           Console.WriteLine(erro.Message);
+           seleção=100; }
+     }   while(seleção!=0);
+       Console.WriteLine ("Acesso finalizado, obrigado!....."); }
+       
  
    public static void PagVendedor() {
      Console.WriteLine("----- Inserção de produtos -----");
@@ -69,10 +87,35 @@ class MainClass {
     int id = int.Parse(Console.ReadLine());
     Console.Write("Informe uma descrição sobre o produto: ");
     string descrição = Console.ReadLine();
-    Categoria c = new Categoria(id, descrição);
-    ncategoria.Inserir(c);  
+    Console.Write("Informe o estoque do produto");
+    int quantidade= int.Parse(Console.ReadLine());
+    Console.Write("Informe o preço do produto em R$ ");
+    double preço= int.Parse(Console.ReadLine());
+    PagCliente();
+    Console.Write("Informe o código da categoria do produto:");
+    int idcategoria=int.Parse(Console.ReadLine());
+    //Seleciona o id da categoria
+    Categoria c= ncategoria.Listar(idcategoria);
+    //instanciando a classe do produto
+    Produto p = new Produto(id, descrição,quantidade,preço,c);
+    //Inserindo o produto no sistema
+    nproduto.Inserir(p);  }
+
+    public static int Usuario() {
+     Console.WriteLine();
+     Console.WriteLine("----------------------------------");
+     Console.WriteLine("Digite uma das opções abaixo para realizar uma ação.");
+
+     Console.WriteLine("1 - Adicionar item ao carrinho");
+     Console.WriteLine("2 - Avaliar o produto");
+     Console.WriteLine("0 - Encerrar o acesso");
+     Console.Write("Informe uma opção: ");
+     int escolha = int.Parse(Console.ReadLine());
+     Console.WriteLine();
+     return escolha;
   }
-}
-}
+   }
+
+
 
                   
