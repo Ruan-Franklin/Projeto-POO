41ca50e4a6374e8ee0358e04ba7986a3014e8c19
RUAN FRANKLIN <94229859+Ruan-Franklin@users.noreply.github.com>
2022-01-24 00:28:53 +0000

    Novas operações de vendedor adicionadas.

---
+++ Cap_3_Jobs/categoria.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Categoria {
+  private int id;
+  private string descrição;
+  private Produto[] produtos = new Produto[10];
+  private int np;
+  public Categoria(int id, string descrição) {
+    this.id = id;
+    this.descrição = descrição;
+  }
+  public void SetId(int id) {
+    this.id = id;
+  }
+  public void SetDescrição(string descrição) {
+    this.descrição = descrição;
+  }
+  public int GetId() {
+    return id;
+  }
+  public string GetDescrição() {
+    return descrição;
+  }
+  public Produto[] ProdutoListar() {
+    Produto[] c = new Produto[np];
+    Array.Copy(produtos, c, np);
+    return c;
+  }
+  public void ProdutoInserir(Produto p) {
+    if (np == produtos.Length) {
+      Array.Resize(ref produtos, 2 * produtos.Length);
+    }
+    produtos[np] = p;
+    np++;
+  }
+  public override string ToString() {
+    return id + " - " + descrição + "  " ;
+  }
+}
