eee89f6c5bed0e54bfeaef3506bdc2633c1002ab
RUAN FRANKLIN <94229859+Ruan-Franklin@users.noreply.github.com>
2022-04-02 15:32:36 +0000

    Adicionando arquivo.cs

---
+++ Cap_15_Page/arquivo.cs
@@ -0,0 +1,5 @@
+//A classe de arquivos manipula um obojeto de um tipo genérico, mas ela precisa fazer uso das bibliotecas de arquivo e de serialização.
+using System;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
